8c058bd78aee2d2df41a0e698c91bda178f1bb0b
sriram835 <sriramtatikonda9@gmail.com>
2024-11-02 09:52:54 +0530

    Created frames in pages

--- src/pages.py
+++ src/pages.py
@@ -53,35 +53,81 @@ def dashboard_page():
 def input_page(main_dict, main_tag):
     root = Tk()
     root.geometry("{}x{}".format(root.winfo_screenwidth(), root.winfo_screenheight()))
-    
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    root.rowconfigure(1, weight=1)
+    root.rowconfigure(2, weight=1)
+    root.rowconfigure(3, weight=1)
+    root.rowconfigure(4, weight=1)
+
+    tag_frame1 = Frame(root)
+    tag_frame1.grid(column=0, row=2)
+
+    tag_frame2 = Frame(root)
+    tag_frame2.grid(column=0, row=1)
+
+    cal_frame = Frame(root)
+    cal_frame.grid(column=0, row=0)
+
     date = int()
-    tag = ""
     tag_button = dict()
     amount = int()
     description = ""
 
     def tag_updation(new_tag):
-        tag_label.configure(state=NORMAL)
-        tag_label.delete(0, END)
-        tag_label.insert(0,new_tag)
-        tag_label.configure(state=DISABLED)
-
+        tag_selection.configure(state=NORMAL)
+        tag_selection.delete(0, END)
+        tag_selection.insert(0,new_tag)
+        tag_selection.configure(state=DISABLED, disabledbackground="white",disabledforeground="black", cursor="arrow")
+        global tag
+        tag = new_tag
     
-    tag_label = Entry(root, state=DISABLED)
-    tag_label.grid(column=0, row=2)
+    
 
+    tag_label = Label(tag_frame2, text="Select the tag")
+    tag_label.grid(column=0, row=0)
+
+    tag_label_selected = Label(tag_frame1, text="Selected tag:")
+    tag_label_selected.grid(column=0, row=0)
+
+    tag_selection = Entry(tag_frame1, state=DISABLED)
+    tag_selection.grid(column=1, row=0)
+
+    tag_updation("")
+    
     # Create a Date Entry widget
-    cal = DateEntry(root, width=12, background="darkblue", foreground="white", borderwidth=2)
+    cal = DateEntry(cal_frame, width=12, background="darkblue", foreground="white", borderwidth=2)
     cal.grid(row=0,column=0)
 
     for i in range(len(main_tag)):
         def action(x = main_tag[i]):
             return tag_updation(x)
         
-        tag_button[main_tag[i]] = Button(root, text=main_tag[i], command=action)
+        tag_button[main_tag[i]] = Button(tag_frame2, text=main_tag[i], command=action)
         tag_button[main_tag[i]].grid(column=i,row=1)
 
+
+    def input_amount():
+        amount = amount_entry.get()
+        return amount
+
+    #amount_entry = Entry(root)
+    #amount_entry.insert(0, "Enter the amount")
+    #amount_entry.bind("<FocusIn>", lambda args: [amount_entry.delete('0', 'end')])
+    #amount_entry.bind("<FocusOut>", lambda args: [input_amount()])
     
+   
+    #amount_entry.grid(column=0, row=4)
+    
+
+    def enter_data():
+        pass
+
+    #enter_data_button = Button(root, text="Enter the data",command=enter_data)
+    #enter_data_button.grid(column=2, row=4)
 
 
     root.mainloop()
+
+
+input_page(sample_dict, sample_tag)
