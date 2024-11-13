b08af15123def3353025cba57c4b9be9c081a857
sriram835 <sriramtatikonda9@gmail.com>
2024-11-13 18:22:04 +0530

    Added comments upto sorting page

--- src/pages.py
+++ src/pages.py
@@ -11,10 +11,10 @@ import matplotlib.pyplot as plt
 #Importing other pages in src folder
 from src.database_dictionary import *
 from src.sorting_dict import *
-from src.input_edit_functions import main
+from src.input_edit_functions import *
 from src.graph_function import *
 
-
+# Sample data for demonstration purposes
 sample_dict = {
     'id': [1,2,3],
     'date': [241101,241102,241103],
@@ -28,25 +28,19 @@ sample_tag = ["snacks","bills","subcriptions"]
 
 
 
-#Creating the dashboard page as a page to be called later in main.py
+
+# Function to create and display the dashboard page, to be called later in main.py
 def dashboard_page():
     #Creating the main window of dashboard page and setting its width and height to full screen
     dash_root = Tk()
     dash_root.config(bg="white")
     dash_root.geometry("{0}x{1}".format(dash_root.winfo_screenwidth(), dash_root.winfo_screenheight()))
+    
     #setting 5 columns and 6 rows in dash root(main window)
-    dash_root.columnconfigure(0, weight=1)
-    dash_root.columnconfigure(1, weight=1)
-    dash_root.columnconfigure(2, weight=1)
-    dash_root.columnconfigure(3, weight=1)
-    dash_root.columnconfigure(4, weight=1)
-    dash_root.rowconfigure(0, weight=1)
-    dash_root.rowconfigure(1, weight=1)
-    dash_root.rowconfigure(2, weight=1)
-    dash_root.rowconfigure(3, weight=1)
-    dash_root.rowconfigure(4, weight=1)
-    dash_root.rowconfigure(5, weight=1)
-    dash_root.rowconfigure(6, weight=1)
+    for i in range(0, 5):
+        dash_root.columnconfigure(i, weight=1)  
+    for i in range(0, 7):
+        dash_root.rowconfigure(i, weight=1)
 
     #Creating a label in the dashboard page with text "dashboard" and setting its font and inserting it in row 0 and column 2
     dashboard_label = Label(dash_root, 
@@ -112,7 +106,7 @@ def dashboard_page():
         )
     tabs_button.grid(row=4, column=3)
 
-
+    #Button to open change tags open
     change_tag_button = Button(dash_root, 
         width= int(dash_root.winfo_screenwidth()*0.015), 
         height= int(dash_root.winfo_screenheight()*0.01),
@@ -126,101 +120,117 @@ def dashboard_page():
         )
     change_tag_button.grid(row=6, column=2)
     
-    
-    
-    
-    
     #Starting a loop to run the dash_root (main window)  
     dash_root.mainloop()
 
 
+
+
+# Function to create and display the input page
 def input_page(main_dict, main_tag, index=0, is_edit=False):
-    
+    # Create the main window for the input page
     input_root = Tk()
-    input_root.geometry("{}x{}".format(input_root.winfo_screenwidth(), input_root.winfo_screenheight()))
+    input_root.geometry("{}x{}".format(input_root.winfo_screenwidth(), 
+                                       input_root.winfo_screenheight()))
+    
+    # Configure column and row weights
     input_root.columnconfigure(0, weight=1)
-    input_root.rowconfigure(0, weight=1)
-    input_root.rowconfigure(1, weight=1)
-    input_root.rowconfigure(2, weight=1)
-    input_root.rowconfigure(3, weight=1)
-    input_root.rowconfigure(4, weight=1)
-    input_root.rowconfigure(5, weight=1)
+    for i in range(0, 6):
+        input_root.rowconfigure(i, weight=1)
 
-
+    # Frame for Date Entry
     cal_frame = Frame(input_root)
     cal_frame.grid(column=0, row=0)
     cal_frame.columnconfigure(0, weight=1)
     cal_frame.columnconfigure(1, weight=1)
     cal_frame.rowconfigure(0, weight=1)
     
+    # Frame for tag selection buttons
     tag_frame2 = Frame(input_root)
     tag_frame2.grid(column=0, row=1)
 
+    # Frame for displaying the selected tag
     tag_frame1 = Frame(input_root)
     tag_frame1.grid(column=0, row=2)
 
+    # Frame for entering amount
     amount_frame =Frame(input_root)
     amount_frame.columnconfigure(0, weight=1)
     amount_frame.columnconfigure(1, weight=1)
     amount_frame.rowconfigure(0, weight=1)
     amount_frame.grid(column=0,row=3)
     
+    # Frame for data entry button 
     enter_data_frame = Frame(input_root)
     enter_data_frame.grid(column=0, row=4)
 
+    # Dictionary to store tag buttons
     tag_button = dict()
     
 
     # Create a Date Entry widget
-    cal = DateEntry(cal_frame, width=12, background="darkblue", foreground="white", borderwidth=2)
+    cal = DateEntry(cal_frame, 
+                    width=12, 
+                    background="darkblue", 
+                    foreground="white", 
+                    borderwidth=2)
     cal.grid(row=0,column=1)
 
+    # Label for date entry
     cal_label = Label(cal_frame, text="Enter date")
     cal_label.grid(column=0, row=0)
 
+    # Function to update the selected tag display
     def tag_updation(new_tag):
         global tag
         tag = new_tag
         tag_selection.configure(state=NORMAL)
         tag_selection.delete(0, END)
         tag_selection.insert(0,new_tag)
-        tag_selection.configure(state=DISABLED, disabledbackground="white",disabledforeground="black", cursor="arrow")
+        tag_selection.configure(state=DISABLED, 
+                                disabledbackground="white", 
+                                disabledforeground="black", 
+                                cursor="arrow")
     
     
-
+    # Labels for tag selection
     tag_label = Label(tag_frame2, text="Select the tag")
     tag_label.grid(column=0, row=0)
 
     tag_label_selected = Label(tag_frame1, text="Selected tag:")
     tag_label_selected.grid(column=0, row=0)
 
+    # Entry to display the selected tag
     tag_selection = Entry(tag_frame1, state=DISABLED)
     tag_selection.grid(column=1, row=0)
 
-    
+    # Initialize with the first tag in the list
     tag_updation(main_tag[0])
 
-
+    # Create tag buttons for each tag in main_tag list
     for i in range(len(main_tag)):
         def action(x = main_tag[i]):
             return tag_updation(x)
         
-        tag_button[main_tag[i]] = Button(tag_frame2, text=main_tag[i], command=action)
+        tag_button[main_tag[i]] = Button(tag_frame2, 
+                                         text=main_tag[i], 
+                                         command=action)
         tag_button[main_tag[i]].grid(column=i,row=1)
 
 
-
+    # Label and Entry for amount input
     amount_label = Label(amount_frame, text="Enter amount: ")
     amount_label.grid(column=0,row=0)
     amount_entry = Entry(amount_frame) 
     amount_entry.grid(column=1, row=0)
     
-
+    # Function to check if data is valid and excute description function afterwards
     def enter_data():
-
+        # Error window for invalid data
         if (amount_entry.get() == "" or not amount_entry.get().isnumeric()):
             root2 = Tk()
-            root2.geometry("{}x{}".format(int(root2.winfo_screenheight()*0.5), int(root2.winfo_screenheight()*0.5)))
+            root2.geometry("{}x{}".format(int(root2.winfo_screenheight()*0.5), 
+                                          int(root2.winfo_screenheight()*0.5)))
             
             message_label = Label(root2, text="Enter proper data")
             message_label.pack()
@@ -229,11 +239,14 @@ def input_page(main_dict, main_tag, index=0, is_edit=False):
 
 
         else:
+            # Store amount and open description entry window
             global amount
             amount = amount_entry.get()
 
+            # New window for description entry
             root2 = Tk()
-            root2.geometry("{}x{}".format(int(root2.winfo_screenheight()), int(root2.winfo_screenheight()*0.5)))
+            root2.geometry("{}x{}".format(int(root2.winfo_screenheight()), 
+                                          int(root2.winfo_screenheight()*0.5)))
             root2.columnconfigure(0, weight=1)
             root2.rowconfigure(0, weight=1)
             root2.rowconfigure(1, weight=1)
@@ -247,21 +260,27 @@ def input_page(main_dict, main_tag, index=0, is_edit=False):
                                     )
             description_entry.grid(column=0, row=1)
 
+            # Finalize entry and call main function with data
             def enter_button():
                     global description
+
+                    # Storing the initial date to year, month, day as integers 
                     uncut_date = str(cal.get_date())
                     uncut_date = uncut_date.split("-")
                     year = int(uncut_date[0][2:4])
                     month = int(uncut_date[1])
                     day = int(uncut_date[2])
 
+                    # Format date as integer in YYMMDD
                     date = year*10000 + month*100 + day
 
+                    # Default description if none entered
                     if (description_entry.get() == ""):
                         description = "None"
                     else:
                         description = description_entry.get()
                     
+                    # Call main function for adding or editing data
                     if (is_edit == True):
                         main(date, tag, amount, description, '2', main_dict, index)
                     else:
@@ -269,23 +288,23 @@ def input_page(main_dict, main_tag, index=0, is_edit=False):
                     
 
                         
-                
+            # Button to store the description entry    
             description_button = Button(root2, text="Enter", command=enter_button)
             description_button.grid(column=0, row=2)
 
 
-
+            
             root2.mainloop()
 
+    # Button to store data, and start decription window
     enter_data_button = Button(enter_data_frame, text="Enter the data",command= lambda: [enter_data(),print(tag)])
     enter_data_button.grid(column=2, row=4)
 
 
     
-
+    # Start main window loop
     input_root.mainloop()
     dashboard_page()
-
 
 
 
@@ -307,45 +326,49 @@ def sorting_page():
     selection_frame.columnconfigure(2, weight=1)
     selection_frame.rowconfigure(0, weight=1)
 
-
-    
+    # Initial sorting settings
     key_selected = "id"
     order_selected = "Ascending"
-    # Change the selected sorting 
+
+    # Function to change the selected sorting key and order, and display sorted items 
     def change_selected_sort(key_selected, order_selected): 
-        key_selected = key_clicked.get()
-        order_selected = order_clicked.get()
+        
+        key_selected = key_clicked.get() # Get the selected key from dropdown
+        order_selected = order_clicked.get() # Get the selected order from dropdown
+        
+        # Sort and display items based on the selected order
         if (order_selected == "Ascending"):
             Sort(main_dict).Ascend_Sorting(key_selected)
-            for i in range(0,len(main_dict["id"])):
-                dict_item(i)
+            
         else:
             Sort(main_dict).Descend_Sorting(key_selected)
-            for i in range(0,len(main_dict["id"])):
-                dict_item(i)
-
+            
+        # Display each item in sorted order    
+        for i in range(0,len(main_dict["id"])):
+            dict_item(i)
+        
     
-    # Dropdown menu options 
+    # Getting main dictionary and options for sorting 
     main_dict = main_dictionary()
     key_options = list(main_dict.keys())
     order_options = ["Ascending","Descending"]
 
-    # datatype of menu text 
+    # Variables to store dropdown menu selections 
     key_clicked = StringVar() 
     order_clicked = StringVar()
     
-    # initial menu text 
+    # Set initial menu text  
     key_clicked.set("id")
     order_clicked.set("Ascending") 
     
-    # Create Dropdown menu 
+    # Create Dropdown menus for key and order, and sorting button 
     order_drop = OptionMenu(selection_frame , order_clicked, *order_options) 
     key_drop = OptionMenu(selection_frame , key_clicked, *key_options) 
     
     # Create button, it start the sorting 
     button = Button( selection_frame , text = "Sort" , command = lambda: change_selected_sort(key_selected, order_selected) )
     
-    #Assigning grid placement to each element
+    # Place dropdown menus and button in grid
     key_drop.grid(column=0, row=0)
     order_drop.grid(column=1, row=0)
     button.grid(column=2, row=0)
@@ -361,24 +384,27 @@ def sorting_page():
     
     sorting_frame0.grid(column=0, row=1)
     sorting_frame0.columnconfigure(0, weight=1)
+    # Configure rows based on number of items in dictionary
     for i in range(0, len(main_dict["id"])+1):
         sorting_frame0.rowconfigure(i, weight=1)
     
-    
-
+    # Dictionary to hold item frames and buttons for displaying full data
     dict_frame = dict()
     full_data_button = dict()
+
+    # Function to display each item in the sorted dictionary
     def dict_item(index):
 
+        # Create a frame for each item
         dict_frame[index] = Frame(sorting_frame0, width=sorting_frame0.winfo_screenwidth())
         dict_frame[index].grid(column=0,row=index+1)
-        dict_frame[index].columnconfigure(0, weight=1)
-        dict_frame[index].columnconfigure(1, weight=1)
-        dict_frame[index].columnconfigure(2, weight=1)
-        dict_frame[index].columnconfigure(3, weight=1)
-        dict_frame[index].columnconfigure(4, weight=1)
+        
+        # Set layout for item information in columns
+        for i in range(0,5):
+            dict_frame[index].columnconfigure(i, weight=1)
         dict_frame[index].rowconfigure(0, weight=1)
 
+        # Function to display the description in a new window
         def display_description(index = index):
             sort_root = Tk()
             sort_root.geometry("{}x{}".format(sort_root.winfo_screenheight(), sort_root.winfo_screenheight()))
@@ -387,12 +413,14 @@ def sorting_page():
             sort_root.mainloop()
             sorting_page()
         
+        # Button to view description
         full_data_button[index] = Button(dict_frame[index], text=">", command=lambda: [sort_root.destroy(),display_description(index)], width=int(dict_frame[index].winfo_screenwidth()*(0.2)))
         full_data_button[index].grid(column=0, row=0)
 
         id_label = Label(dict_frame[index], text=main_dict['id'][index], width=int(dict_frame[index].winfo_screenwidth()*(9/40)))
         id_label.grid(column=1, row=0)
 
+        # Converting date from YYMMDD to DD/MM/YY
         date = str(main_dict['date'][index])
         day = date[4:6]
         month = date[2:4]
@@ -406,44 +434,45 @@ def sorting_page():
         amount_label = Label(dict_frame[index], text=main_dict["amount"][index], width=int(dict_frame[index].winfo_screenwidth()*(9/40)))
         amount_label.grid(column=4,row=0)
 
-
-
-    
+    # Frame header for displaying labels of the columns (id, date, tag, amount)
     dict_frame0 = Frame(sorting_frame0)
-
     dict_frame0.grid(column=0,row=0)
-    dict_frame0.columnconfigure(0, weight=1)
-    dict_frame0.columnconfigure(1, weight=1)
-    dict_frame0.columnconfigure(2, weight=1)
-    dict_frame0.columnconfigure(3, weight=1)
-    dict_frame0.columnconfigure(4, weight=1)
+    for i in range(0, 5):
+        dict_frame0.columnconfigure(i, weight=1)
     dict_frame0.rowconfigure(0, weight=1)
-    full_data_button0 = Button(dict_frame0, text=">", width=int(dict_frame0.winfo_screenwidth()*0.2))
+
+    # Header labels for the columns
+    full_data_button0 = Button(dict_frame0, 
+                               text=">", 
+                               width=int(dict_frame0.winfo_screenwidth()*0.2))
     full_data_button0.grid(column=0, row=0)
-
-    id_label0 = Label(dict_frame0, text="id",width=int(dict_frame0.winfo_screenwidth()*(9/40)))
+    id_label0 = Label(dict_frame0, 
+                      text="id",
+                      width=int(dict_frame0.winfo_screenwidth()*(9/40)))
     id_label0.grid(column=1, row=0)
-
-    date_label0 = Label(dict_frame0, text="date",width=int(dict_frame0.winfo_screenwidth()*(9/40)))
+    date_label0 = Label(dict_frame0, 
+                        text="date",
+                        width=int(dict_frame0.winfo_screenwidth()*(9/40)))
     date_label0.grid(column=2,row=0)
-
-    tag_label0 = Label(dict_frame0, text="tags",width=int(dict_frame0.winfo_screenwidth()*(9/40)))
+    tag_label0 = Label(dict_frame0, 
+                       text="tags",
+                       width=int(dict_frame0.winfo_screenwidth()*(9/40)))
     tag_label0.grid(column=3,row=0)
-
-    amount_label0 = Label(dict_frame0, text="amount",width=int(dict_frame0.winfo_screenwidth()*(9/40)))
+    amount_label0 = Label(dict_frame0, 
+                          text="amount",
+                          width=int(dict_frame0.winfo_screenwidth()*(9/40)))
     amount_label0.grid(column=4,row=0)
     
+    # Perform sorting and display items based on initial selected order
     if (order_selected == "Ascending"):
         Sort(main_dict).Ascend_Sorting(key_selected)
-        for i in range(0,len(main_dict["id"])):
-            dict_item(i)
     else:
         Sort(main_dict).Descend_Sorting(key_selected)
-        for i in range(0,len(main_dict["id"])):
+
+    for i in range(0,len(main_dict["id"])):
             dict_item(i)
 
-
-    # Execute tkinter 
+    # Start the Tkinter main loop 
     sort_root.mainloop()
     dashboard_page()     
 
